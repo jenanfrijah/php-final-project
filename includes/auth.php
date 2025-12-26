fb9a9b62f9ea29590756320f4b5b1f122de845aa
jenanfrijah <frijahjenan@gmail.com>
2025-12-26 18:57:49 +0300

    authuntication added

---
+++ includes/auth.php
@@ -0,0 +1,85 @@
+<?php
+
+if(session_status() === PHP_SESSION_NONE){
+       session_start();
+}
+//to make sure that the user is logged in 
+function isLoggedIn(){
+    return isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
+}
+
+function isAdmin(){
+    return isLoggedIn() && isset($_SESSION['role']) && $_SESSION['role']==='admin';
+}
+
+function getCurrentUser(){
+    if(!isLoggedIn()){
+        return null;
+    }
+
+    return [
+        'user_id'=> $_SESSION['user_id'],
+        'first_name'=> $_SESSION['first_name'],
+        'last_name'=> $_SESSION['last_name'],
+        'email'=> $_SESSION['email'],
+        'location'=> $_SESSION['location'] ?? '',
+        'role'=> $_SESSION['role']
+    ];
+}
+
+function getFullName(){
+    if(!isLoggedIn()){
+        return '';
+    }
+
+    $first_name = $_SESSION['first_name'];
+    $last_name = $_SESSION['last_name'];
+
+    return trim($first_name.' '.$last_name);
+
+}
+
+
+function login($user_id,$first_name,$last_name,$email,$location='',$role='user'){
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['first_name'] = $first_name;
+    $_SESSION['last_name'] = $last_name;
+    $_SESSION['email'] = $email;
+    $_SESSION['location'] = $location;
+    $_SESSION['role'] = $role;
+    $_SESSION['login_time'] = time();
+}
+
+
+
+
+function logout(){
+
+    $_SESSION =array();
+    
+    if(isset($_COOKIE[session_name()])){
+        setcookie(session_name(),'',time()-3600,'/');
+    }
+
+    session_destroy();
+}
+
+function requireLogin ($redirectURL='public/login.php'){
+    if(!isLoggedIn()){
+        header("Location: $redirectURL");
+        exit();
+    }
+}
+
+
+function requireAdmin($redirectURL='public/index.php'){
+    if(!isAdmin()){
+        header("Location: $redirectURL");
+        exit();
+    }
+}
+
+
+
+
+?>
